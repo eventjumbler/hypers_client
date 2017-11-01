1b1e24441a1401bab79754ea7dedeaa8f824094c
zero <sontt246@gmail.com>
2017-11-01 18:18:09 +0700

    Update setup and dependencies

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
+import re
 import pip
 from setuptools import setup, find_packages
 
+
 _LINKS = []  # for repo urls (dependency_links)
 _REQUIRES = []  # for package names
+
+
+def __normalize(req):
+    # Strip off -dev, -0.2, etc.
+    match = re.search(r'^(.*?)(?:-dev|-\d.*)$', req)
+    return match.group(1) if match else req
 
 requirements = pip.req.parse_requirements(
     'requirements.txt', session=pip.download.PipSession()
 )
 
 for item in requirements:
-    print(item)
+    has_link = False
     if getattr(item, 'url', None):
         _LINKS.append(str(item.url))
+        has_link = True
     if getattr(item, 'link', None):
         _LINKS.append(str(item.link))
+        has_link = True
     if item.req:
-        _REQUIRES.append(str(item.req))
+        req = str(item.req)
+        _REQUIRES.append(__normalize(req) if has_link else req)
 
 setup(
     name="hypersh-client",
